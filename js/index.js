bbccb6a47b6321aaed5f099b5f02cf5414f32c28
leonel <lionel.lalo@hotmail.com>
2022-11-13 20:25:21 -0500

    add filter by category

--- js/index.js
+++ js/index.js
@@ -1,15 +1,20 @@
 //Definición de variables
 const url = 'http://localhost:8000/api/products'
-const urlSearch = 'http://localhost:8000/api/products/search?name='
+const urlCategories = 'http://localhost:8000/api/categories'
 const items = document.getElementById('items')
 const templateCard = document.getElementById('template-card').content
 const fragment = document.createDocumentFragment()
 const inputBuscar = document.getElementById('buscar')
 const celdas = document.getElementsByTagName('h5')
+const select = document.querySelector("#miSelect");
+
+let valueSearch = ""
+let valueSelect = "all"
 
 //funcion para mostrar los productos
 const mostrar = (elements) => {
     document.getElementById('items').innerHTML = ""
+    
     elements.forEach(element => {
         templateCard.querySelector('h5').textContent = element.name
         templateCard.querySelector('img').src = element.url_image
@@ -21,35 +26,57 @@ const mostrar = (elements) => {
 
 }
 
-//Procedimiento Mostrar
+//funcion para mostrar las categorias
+const mostrarCategorias = (elements) => {
+
+    elements.forEach(element => {
+        const option = document.createElement('option');
+        
+        option.value = element.id;
+        option.text = element.name;
+        select.appendChild(option);
+    })
+
+}
+
+//Procedimiento Mostrar los productos
 fetch(url)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
         mostrar(data.products)
     })
     .catch(error => console.log(error))
 
+//Procedimiento Mostrar las categorias
+fetch(urlCategories)
+    .then(response => response.json())
+    .then(data => {
+        mostrarCategorias(data.categories)
+    })
+    .catch(error => console.log(error))
+
+
 //Búsqueda
 inputBuscar.addEventListener('keyup', (e)=>{
     let texto = e.target.value
-    console.log(texto)
-    fetch(urlSearch+texto)
+    valueSearch = texto
+    const urlById = 'http://localhost:8000/api/products/search?name='+texto+'&categoryId='+valueSelect
+    fetch(urlById)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
         mostrar(data.products)
     })
     .catch(error => console.log(error))
-    // let er = new RegExp(texto, "i")
-    // for(let i=0; i<celdas.length; i++) {
-    //     let valor = celdas[i]
-    //     console.log(valor)
-    //     if(er.test(valor.innerText)){
-    //         valor.classList.remove("ocultar")
-    //     }else{
-    //         console.log(valor)
-    //         valor.classList.add("ocultar")
-    //     }
-    // }
+    
+})
+select.addEventListener('change', (e)=>{
+    let id = e.target.value
+    valueSelect = id
+    const urlById = 'http://localhost:8000/api/products/search?name='+valueSearch+'&categoryId='+id
+    fetch(urlById)
+    .then(response => response.json())
+    .then(data => {
+        mostrar(data.products)
+    })
+    .catch(error => console.log(error))
 })
